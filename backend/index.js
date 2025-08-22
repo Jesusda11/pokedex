2531c372f7c582697664118bac2b6fdbebced67f
sukaNico <nicolaslol151@gmail.com>
2025-08-21 21:02:36 -0500

    estructura basica fronted

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
 console.log('Hola mudo');
+
+import axios from 'axios';
+
+console.log("hi");
+
+
+axios.get('https://pokeapi.co/api/v2/pokemon/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
---
+++ frontend/js/main.js
@@ -0,0 +1,3 @@
+axios.get("https://pokeapi.co/api/v2/pokemon/pikachu")
+  .then(response => console.log(response.data))
+  .catch(error => console.error(error));
